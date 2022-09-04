6c8c8d1e22d270f891a486a095ff653231946b8f
vs <vs@playa.tech>
2022-09-04 22:41:23 +0300

    add ht template for lecture 02

---
+++ lect_03/ht_template/home_task_queries.sql
@@ -0,0 +1,48 @@
+/*
+ Завдання на SQL до лекції 02.
+ */
+
+
+/*
+1.
+
+*/
+-- SQL code goes here...
+
+
+
+/*
+2.
+
+*/
+-- SQL code goes here...
+
+
+
+/*
+3.
+
+*/
+-- SQL code goes here...
+
+
+
+/*
+4.
+
+*/
+-- SQL code goes here...
+
+
+/*
+5.
+
+*/
+-- SQL code goes here...
+
+
+/*
+6.
+
+*/
+-- SQL code goes here...
