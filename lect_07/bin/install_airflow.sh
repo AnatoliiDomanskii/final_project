52dc919be373b57683ae8bd37dc82be9e20b045d
vs <vs@playa.tech>
2022-09-19 16:57:52 +0300

    upgrade airflow scripts

--- lect_07/bin/install_airflow.sh
+++ lect_07/bin/install_airflow.sh
@@ -11,4 +11,4 @@ echo
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 echo "Constrained URL used: ${CONSTRAINT_URL}"
 echo
-#pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
