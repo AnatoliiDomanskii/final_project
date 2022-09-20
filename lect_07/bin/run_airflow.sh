1d239d1c3c07db0fea8f3b6409656cb45ff89889
vs <vs@playa.tech>
2022-09-20 23:15:01 +0300

    change airflow home

--- lect_07/bin/run_airflow.sh
+++ lect_07/bin/run_airflow.sh
@@ -2,5 +2,6 @@
 # NB:install Apache Airflow first using install_airflow.sh script
 
 # TODO: Change this to the path where airflow directory is located
-export AIRFLOW_HOME=~/airflow
+# (default is ~/airflow)
+export AIRFLOW_HOME=/Users/hunting/projects/r_d/DE2022/lect_07/airflow
 airflow standalone
