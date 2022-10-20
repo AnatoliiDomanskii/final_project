81885eabb3bf88401da5b0331f28e82c03c74f4e
vs <vs@playa.tech>
2022-10-20 22:04:56 +0300

    add sales pipeline

--- lect_07/bin/run_airflow.sh
+++ lect_07/bin/run_airflow.sh
@@ -4,4 +4,6 @@
 # TODO: Change this to the path where airflow directory is located
 # (default is ~/airflow)
 export AIRFLOW_HOME=/Users/hunting/projects/r_d/DE2022/lect_07/airflow
+# fixes issue on Mac:
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 airflow standalone
